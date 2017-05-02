08dd08385e453e8befc45b47e230d718529bdac5
kobblander <kobblander@gmail.com>
2017-05-02 17:09:40 +0000

    oops forgot to add

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Create symlinks for the scripts
 sudo ln -sfv $DIR/scripts/* /usr/local/bin
 
--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-	find ~/Pictures/wallpapers/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+	find ~/Pictures/activeWallpapers/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
 		shuf -n1 -z | xargs -0 feh --bg-fill
 	sleep 30s
 done
