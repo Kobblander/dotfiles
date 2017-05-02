2725d9aaf5c4555894e0a2f28ccc767a114542f7
kobblander <kobblander@gmail.com>
2017-05-02 17:07:02 +0000

    added bash color tests and wallpaper update

---
+++ echotest.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function errorbox() {
+    echo -n "\t[${RED}x${NC}] "
+}
+
+function successbox() {
+    echo -n "\t[${GREEN}✓${NC}] "
+}
+
+echo "Installing..."
+
+errorbox; echo "Foobar"
+successbox; echo "Polybar"
+successbox; echo "Git"
+errorbox; echo "Stackoverflow"
+successbox; echo "i3"
