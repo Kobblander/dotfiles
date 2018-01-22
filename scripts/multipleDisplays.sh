a562b5891e51d44964756ab8b8703ccb02b59c94
kobblander <kobblander@gmail.com>
2018-01-22 08:56:00 +0000

    loads of changes

--- scripts/multipleDisplays.sh
+++ scripts/multipleDisplays.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 xrandr  \
-    --output DP-2-1 --mode 1920x1080 \
-    --output DP-1 --auto --left-of DP-2-1 --rotate left\
-    --output DP-2-2 --auto --left-of DP-1 --rotate left\
+    --output DP-2-2 --mode 1920x1080 \
+    --output DP-1 --auto --left-of DP-2-2 --rotate left\
+    --output DP-2-1 --auto --left-of DP-1 --rotate left\
